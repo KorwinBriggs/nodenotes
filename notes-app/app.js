7090841ae0c9e64cdff0245395a6580555557ff3
Korwin Briggs <Korwin.Briggs@gmail.com>
2021-07-06 14:42:25 -0400

    Adding yargs and placeholder commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,11 +3,46 @@
 // const getNotes = require('./notes.js')
 
 import chalk from 'chalk'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 import notes from './notes.js'
 
-const msg = notes.getNotes()
+const yar = yargs(hideBin(process.argv))
 
-console.log(msg)
+// console.log(yargs(hideBin(process.argv)).argv)
+// yargs.length;
+// yargs().version()
+//commands: add, remove, read, list
 
-console.log(chalk.green.bold.italic('Success!'))
+// create add command
+yar.command({
+        command: "add",
+        describe: "Add a new note",
+        handler: () => {
+            console.log("Note added")
+         }
+    })
+    .command({
+        command: "remove",
+        describe: "Remove a note",
+        handler: () => {
+            console.log("Note removed")
+         }
+    })
+    .command({
+        command: "list",
+        describe: "Print list of notes",
+        handler: () => {
+            console.log("Listing notes...")
+        }
+    })
+    .command({
+        command: "read",
+        describe: "Read a note",
+        handler: () => {
+            console.log("Displaying note...")
+        }
+    })
+    .argv
 
+// yargs().parse();
